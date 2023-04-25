530fccc21a6f4b30b6408f69d359f73f1a0bb18b
Jae-Young98 <ygy0102@gmail.com>
2023-04-25 21:32:35 +0900

    두 수의 합(투 포인터)

---
+++ src/BOJ/BOJ_3273.java
@@ -0,0 +1,42 @@
+package BOJ;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+import java.util.Arrays;
+
+public class BOJ_3273 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        int n = Integer.parseInt(br.readLine());
+        st = new StringTokenizer(br.readLine()," ");
+        int x = Integer.parseInt(br.readLine());
+        int start = 0;
+        int end = n-1;
+        int sum;
+        int cnt = 0;
+
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(arr);
+
+        while(start < end) {
+            sum = arr[start] + arr[end];
+            if (sum == x) {
+                cnt++;
+                start++;
+            } else if (sum > x) {
+                end--;
+            } else {
+                start++;
+            }
+        }
+        br.close();
+        System.out.println(cnt);
+    }
+}
