cbd6fefe44d601345e86a76a9a417d8377822cc8
Jae-Young98 <ygy0102@gmail.com>
2023-04-28 16:22:49 +0900

    폴더 정리

--- src/BOJ/BarkingDogAlgorithm/Array/BOJ_3273.java
+++ src/BOJ/BarkingDogAlgorithm/Array/BOJ_3273.java
@@ -1,4 +1,4 @@
-package BOJ;
+package BOJ.BarkingDogAlgorithm.Array;
 
 import java.io.BufferedReader;
 import java.io.IOException;
--- src/BOJ/BarkingDogAlgorithm/LinkedList/BOJ_5397.java
+++ src/BOJ/BarkingDogAlgorithm/LinkedList/BOJ_5397.java
@@ -1,4 +1,4 @@
-package BOJ;
+package BOJ.BarkingDogAlgorithm.LinkedList;
 
 import java.io.BufferedReader;
 import java.io.IOException;
