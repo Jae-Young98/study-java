9f137ba5d8fba3da44ea3b8ea69d0b14076e213f
Jae-Young98 <ygy0102@gmail.com>
2023-05-08 02:49:18 +0900

    문자열 반복

---
+++ src/BOJ/Basic/BOJ_2675.java
@@ -0,0 +1,29 @@
+package BOJ.Basic;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ_2675 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+
+        int n = Integer.parseInt(br.readLine());
+        for (int i = 0; i < n; i ++) {
+            st = new StringTokenizer(br.readLine()," ");
+            int r = Integer.parseInt(st.nextToken());
+            String s = st.nextToken();
+
+            for (int j = 0; j < s.length(); j++) {
+                for (int k = 0; k < r; k++) {
+                    sb.append(s.charAt(j));
+                }
+            }
+            sb.append('\n');
+        }
+        System.out.println(sb);
+    }
+}
