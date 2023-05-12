c831c895ca5a72eb8dfde7c7b6cbc0679a2e92a1
Jae-Young98 <ygy0102@gmail.com>
2023-05-13 00:11:26 +0900

    AC

--- src/BOJ/BarkingDogAlgorithm/Deque/BOJ_5430.java
+++ src/BOJ/BarkingDogAlgorithm/Deque/BOJ_5430.java
@@ -3,7 +3,6 @@ package BOJ.BarkingDogAlgorithm.Deque;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayDeque;
 import java.util.Deque;
 import java.util.LinkedList;
 import java.util.StringTokenizer;
