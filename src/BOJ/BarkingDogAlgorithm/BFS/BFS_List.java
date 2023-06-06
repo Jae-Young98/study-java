4716baabe62e61d4c20c31c479cf8dd4056405af
Jae-Young98 <ygy0102@gmail.com>
2023-06-06 22:21:56 +0900

    BFS 인접리스트 구현

---
+++ src/BOJ/BarkingDogAlgorithm/BFS/BFS_List.java
@@ -0,0 +1,58 @@
+package BOJ.BarkingDogAlgorithm.BFS;
+
+import java.util.*;
+
+public class BFS_List {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt(); // 정점의 개수
+        int m = sc.nextInt(); // 간선의 개수
+        int v = sc.nextInt(); // 탐색 시작할 정점의 번호
+
+        boolean visited[] = new boolean[n + 1];
+
+        LinkedList<Integer>[] adjList = new LinkedList[n + 1];
+
+        for (int i = 0; i <= n; i++) {
+            adjList[i] = new LinkedList<Integer>();
+        }
+
+        // 두 정점 사이에 여러 개의 간선이 있을 수 있음
+        // 입력으로 주어지는 간선은 양방향
+        for (int i = 0; i < m; i++) {
+            int v1 = sc.nextInt();
+            int v2 = sc.nextInt();
+            adjList[v1].add(v2);
+            adjList[v2].add(v1);
+        }
+
+        for (int i = 0; i <= n; i++) {
+            Collections.sort(adjList[i]); // 방문 순서를 위해 정렬
+        }
+
+        System.out.println("BFS 인접리스트");
+        bfsList(v, adjList, visited);
+
+    }
+
+    public static void bfsList(int v, LinkedList<Integer>[] adjList, boolean[] visited) {
+        Queue<Integer> queue = new LinkedList<Integer>();
+        visited[v] = true;
+        queue.add(v);
+
+        while (queue.size() != 0) {
+            v = queue.poll();
+            System.out.print(v + " ");
+
+            Iterator<Integer> iter = adjList[v].listIterator();
+            while (iter.hasNext()) {
+                int w = iter.next();
+                if (!visited[w]) {
+                    visited[w] = true;
+                    queue.add(w);
+                }
+            }
+        }
+    }
+}
