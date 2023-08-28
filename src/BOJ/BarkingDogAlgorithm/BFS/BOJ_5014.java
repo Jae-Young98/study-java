bb3a252fb1888060ac030ab4a9fddd5b983fdaa7
Jae-Young98 <ygy0102@gmail.com>
2023-08-28 20:23:09 +0900

    스타트 링크

---
+++ src/BOJ/BarkingDogAlgorithm/BFS/BOJ_5014.java
@@ -0,0 +1,58 @@
+package BOJ.BarkingDogAlgorithm.BFS;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class BOJ_5014 {
+    static int f, s, g, u, d; // 총 층수, 시작, 목표 층, 상승, 하강
+    static int cnt;
+    static int[] arr;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        f = Integer.parseInt(st.nextToken());
+        s = Integer.parseInt(st.nextToken());
+        g = Integer.parseInt(st.nextToken());
+        u = Integer.parseInt(st.nextToken());
+        d = Integer.parseInt(st.nextToken());
+
+        arr = new int[f + 1];
+        int result = bfs(s);
+        if (result == -1) {
+            System.out.println("use the stairs");
+        } else {
+            System.out.println(result - 1);
+        }
+    }
+
+    public static int bfs(int x) {
+        Queue<Integer> queue = new LinkedList<>();
+        queue.offer(x);
+        arr[s] = 1;
+
+        while (!queue.isEmpty()) {
+            int position = queue.poll(); // 현재
+            if (position == g) {
+                return arr[position];
+            }
+
+            if (position + u <= f) {
+                if (arr[position + u] == 0) {
+                    arr[position + u] = arr[position] + 1;
+                    queue.offer(position + u);
+                }
+            }
+            if (position - d > 0) {
+                if (arr[position - d] == 0) {
+                    arr[position - d] = arr[position] + 1;
+                    queue.offer(position - d);
+                }
+            }
+        }
+        return -1;
+    }
+}
