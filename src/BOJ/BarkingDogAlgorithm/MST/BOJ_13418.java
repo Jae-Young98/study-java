4f09f4efa52c3631cd8da0dbfa5b12475d2a5e7f
Jae-Young98 <ygy0102@gmail.com>
2023-10-08 23:12:45 +0900

    학교 탐방하기

---
+++ src/BOJ/BarkingDogAlgorithm/MST/BOJ_13418.java
@@ -0,0 +1,106 @@
+package BOJ.BarkingDogAlgorithm.MST;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Collections;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
+public class BOJ_13418 {
+
+    static int[] parent;
+    static int n, m;
+    static PriorityQueue<node> worstQueue;
+    static PriorityQueue<node> queue;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        worstQueue = new PriorityQueue<>();
+        queue = new PriorityQueue<>(Collections.reverseOrder());
+
+        parent = new int[n + 1];
+        for (int i = 0; i <= n; i++) {
+            parent[i] = i;
+        }
+
+        for (int i = 0; i <= m; i++) {
+            st = new StringTokenizer(br.readLine());
+            int s = Integer.parseInt(st.nextToken());
+            int e = Integer.parseInt(st.nextToken());
+            int v = Integer.parseInt(st.nextToken());
+
+            worstQueue.add(new node(s, e, v));
+            queue.add(new node(s, e, v));
+        }
+
+        // 최악의 경우
+        int worstUphill = 0;
+        while (!worstQueue.isEmpty()) {
+            node now = worstQueue.poll();
+
+            if (find(now.s) != find(now.e)) {
+                union(now.s, now.e);
+
+                if (now.v == 0) {
+                    worstUphill++;
+                }
+            }
+        }
+
+        for (int i = 0; i <= n; i++) {
+            parent[i] = i;
+        }
+
+        // 최적의 경우
+        int uphill = 0;
+        while (!queue.isEmpty()) {
+            node now = queue.poll();
+
+            if (find(now.s) != find(now.e)) {
+                union(now.s, now.e);
+
+                if (now.v == 0) {
+                    uphill++;
+                }
+            }
+        }
+        int result = (int) (Math.pow(worstUphill, 2) - Math.pow(uphill, 2));
+        System.out.println(result);
+    }
+
+    public static void union(int a, int b) {
+        a = find(a);
+        b = find(b);
+        if (a != b) {
+            parent[b] = a;
+        }
+    }
+
+    public static int find(int a) {
+        if (parent[a] == a) {
+            return a;
+        } else {
+            return parent[a] = find(parent[a]);
+        }
+    }
+
+    static class node implements Comparable<node> {
+        int s;
+        int e;
+        int v;
+
+        node(int s, int e, int v) {
+            this.s = s;
+            this.e = e;
+            this.v = v;
+        }
+
+        public int compareTo(node o) {
+            return this.v - o.v;
+        }
+    }
+}
