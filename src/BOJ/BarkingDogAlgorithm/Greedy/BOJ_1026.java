598ac25266e906c9be6911c79d119ea201069ab5
Jae-Young98 <ygy0102@gmail.com>
2023-10-18 22:54:40 +0900

    보물

---
+++ src/BOJ/BarkingDogAlgorithm/Greedy/BOJ_1026.java
@@ -0,0 +1,44 @@
+package BOJ.BarkingDogAlgorithm.Greedy;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.StringTokenizer;
+
+public class BOJ_1026 {
+
+    static int N;
+    static int[] A;
+    static Integer[] B;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        N = Integer.parseInt(br.readLine());
+        A = new int[N];
+        B = new Integer[N];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            A[i] = Integer.parseInt(st.nextToken());
+        }
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            B[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(A);
+        Arrays.sort(B, Comparator.reverseOrder());
+        int result = 0;
+        for (int i = 0; i < N; i++) {
+            int s = A[i] * B[i];
+            result += s;
+        }
+
+        System.out.println(result);
+    }
+}
