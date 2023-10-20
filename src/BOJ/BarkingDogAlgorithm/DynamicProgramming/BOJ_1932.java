777d4a002efc669017b637d8d643c891c044950f
Jae-Young98 <ygy0102@gmail.com>
2023-10-21 04:27:05 +0900

    정수 삼각형

---
+++ src/BOJ/BarkingDogAlgorithm/DynamicProgramming/BOJ_1932.java
@@ -0,0 +1,41 @@
+package BOJ.BarkingDogAlgorithm.DynamicProgramming;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ_1932 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        int[][] arr;
+        int[][] dp;
+
+        int N = Integer.parseInt(br.readLine());
+        arr = new int[N+1][N+1];
+        dp = new int[N+1][N+1];
+
+        for (int i = 1; i <= N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 1; j <= i; j++) {
+                arr[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        for (int i = 1; i <= N; i++) {
+            for (int j = 1; j <= N; j++) {
+                dp[i][j] = Math.max(dp[i-1][j-1], dp[i-1][j]) + arr[i][j];
+            }
+        }
+
+        int result = 0;
+        for (int i = 1; i <= N; i++) {
+            if (result < dp[N][i]) {
+                result = dp[N][i];
+            }
+        }
+
+        System.out.println(result);
+    }
+}
