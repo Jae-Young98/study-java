a283c473af783b8b3475d9547064bb79ad5dc8f3
Jae-Young98 <ygy0102@gmail.com>
2023-10-21 17:51:03 +0900

    뒤집기

---
+++ src/BOJ/BarkingDogAlgorithm/Greedy/BOJ_1439.java
@@ -0,0 +1,32 @@
+package BOJ.BarkingDogAlgorithm.Greedy;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BOJ_1439 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int zero = 0;
+        int one = 0;
+
+        String str = br.readLine();
+        if (str.charAt(0) == '0') {
+            zero++;
+        } else {
+            one++;
+        }
+
+        for (int i = 1; i < str.length(); i++) {
+            if (str.charAt(i - 1) != str.charAt(i)) {
+                if (str.charAt(i) == '0') {
+                    zero++;
+                } else {
+                    one++;
+                }
+            }
+        }
+        System.out.println(Math.min(zero, one));
+    }
+}
