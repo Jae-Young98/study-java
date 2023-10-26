4701192d8b617193272761dc76a47971b6eddc21
Jae-Young98 <ygy0102@gmail.com>
2023-10-27 00:35:25 +0900

    좌표 정렬하기

---
+++ src/BOJ/Sort/BOJ_11650.java
@@ -0,0 +1,53 @@
+package BOJ.Sort;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class BOJ_11650 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+        List<Point> list = new ArrayList<>();
+
+        int N = Integer.parseInt(br.readLine());
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+            list.add(new Point(x, y));
+        }
+
+        Collections.sort(list);
+
+        for (Point p : list) {
+            sb.append(p.x + " ").append(p.y).append('\n');
+        }
+
+        System.out.println(sb);
+
+    }
+
+    private static class Point implements Comparable<Point> {
+        int x;
+        int y;
+
+        Point(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+
+        @Override
+        public int compareTo(Point p) {
+            if (this.x == p.x) {
+                return this.y - p.y;
+            }
+            return this.x - p.x;
+        }
+    }
+}
