c6f09ec2a8578abc220c15bd96cbe363a8ae1acc
Jae-Young98 <ygy0102@gmail.com>
2023-11-18 15:12:43 +0900

    좌표 정렬하기 2

---
+++ src/BOJ/Sort/BOJ_11651.java
@@ -0,0 +1,49 @@
+package BOJ.Sort;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class BOJ_11651 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        int N = Integer.parseInt(br.readLine());
+        List<Point> list = new ArrayList<>();
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+            list.add(new Point(x, y));
+        }
+        Collections.sort(list);
+
+        for (Point p : list) {
+            System.out.print(p.x + " " + p.y);
+            System.out.println();
+        }
+    }
+
+    public static class Point implements Comparable<Point> {
+        int x;
+        int y;
+
+        Point(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+
+        @Override
+        public int compareTo(Point o) {
+            if (this.y == o.y) {
+                return this.x - o.x;
+            }
+            return this.y - o.y;
+        }
+    }
+}
