e4da1a9101ff96f12bec0ad348bd9326737b8eea
Jae-Young98 <ygy0102@gmail.com>
2023-12-06 23:24:06 +0900

    오등큰수

--- src/BOJ/BarkingDogAlgorithm/Stack/BOJ_17299.java
+++ src/BOJ/BarkingDogAlgorithm/Stack/BOJ_17299.java
@@ -3,6 +3,7 @@ package BOJ.BarkingDogAlgorithm.Stack;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Stack;
 import java.util.StringTokenizer;
 
 public class BOJ_17299 {
@@ -23,6 +24,21 @@ public class BOJ_17299 {
             count[x]++;
         }
 
+        Stack<Integer> stack = new Stack<>();
+        for (int i = 0; i < N; i++) {
+            while (!stack.isEmpty() && count[arr[stack.peek()]] < count[arr[i]]) {
+                result[stack.pop()] = arr[i];
+            }
+            stack.push(i);
+        }
 
+        while (!stack.isEmpty()) {
+            result[stack.pop()] = -1;
+        }
+
+        for (int i = 0; i < N; i++) {
+            sb.append(result[i]).append(' ');
+        }
+        System.out.println(sb);
     }
 }
