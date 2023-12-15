3233a780543e88a3372833a73b5630b904181a57
Jae-Young98 <ygy0102@gmail.com>
2023-12-15 19:11:09 +0900

    최대공약수와 최소공배수

---
+++ src/BOJ/GeneralMath1/BOJ_2609.java
@@ -0,0 +1,31 @@
+package BOJ.GeneralMath1;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ_2609 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        if (b > a) {
+            int tmp = b;
+            b = a;
+            a = tmp;
+        }
+        int result = gcd(a, b);
+        System.out.println(result);
+        System.out.println(a * b / result);
+    }
+
+    public static int gcd(int a, int b) {
+        if (a % b == 0) {
+            return b;
+        }
+        return gcd(b, a % b);
+    }
+}
