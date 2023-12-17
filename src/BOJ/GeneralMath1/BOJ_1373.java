b07257c857a4566436ce83f820f5db8de65014a5
Jae-Young98 <ygy0102@gmail.com>
2023-12-17 21:36:01 +0900

    2진수 8진수

---
+++ src/BOJ/GeneralMath1/BOJ_1373.java
@@ -0,0 +1,26 @@
+package BOJ.GeneralMath1;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BOJ_1373 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        String str = br.readLine();
+        if (str.length() % 3 == 1) {
+            sb.append(str.charAt(0));
+        } else if (str.length() % 3 == 2) {
+            sb.append((str.charAt(0) - '0') * 2 + (str.charAt(1) - '0'));
+        }
+        for (int i = str.length() % 3; i < str.length(); i += 3) {
+            int a = (str.charAt(i) - '0') * 4;
+            int b = (str.charAt(i + 1) - '0') * 2;
+            int c = (str.charAt(i + 2) - '0');
+            sb.append(a + b + c);
+        }
+        System.out.print(sb);
+    }
+}
