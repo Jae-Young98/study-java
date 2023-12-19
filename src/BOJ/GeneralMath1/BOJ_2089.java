b599d00d6aa79cf08d362f8b31b0bcaeaa91dd89
Jae-Young98 <ygy0102@gmail.com>
2023-12-20 00:37:09 +0900

    -2진수

---
+++ src/BOJ/GeneralMath1/BOJ_2089.java
@@ -0,0 +1,25 @@
+package BOJ.GeneralMath1;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BOJ_2089 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int N = Integer.parseInt(br.readLine());
+
+        if (N == 0) {
+            System.out.println(0);
+            return;
+        }
+
+        while (N != 0) {
+            sb.append(Math.abs(N % -2));
+            N = (int) Math.ceil((double) N / -2);
+        }
+
+        System.out.print(sb.reverse());
+    }
+}
