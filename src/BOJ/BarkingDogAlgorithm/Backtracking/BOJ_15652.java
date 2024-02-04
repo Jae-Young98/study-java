3bed72ada0c3077c2edb990cec30e694fb0cc4dd
Jae-Young98 <ygy0102@gmail.com>
2024-02-04 16:20:39 +0900

    N과 M(4)

---
+++ src/BOJ/BarkingDogAlgorithm/Backtracking/BOJ_15652.java
@@ -0,0 +1,38 @@
+package BOJ.BarkingDogAlgorithm.Backtracking;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ_15652 {
+
+    static int n, m;
+    static int[] arr;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        arr = new int[m];
+
+        dfs(1, 0);
+    }
+
+    public static void dfs(int start, int depth) {
+        if (depth == m) {
+            for (int i = 0; i < m; i++) {
+                System.out.print(arr[i] + " ");
+            }
+            System.out.println();
+            return;
+        }
+
+        for (int i = start; i <= n; i++) {
+            arr[depth] = i;
+            dfs(i, depth + 1);
+        }
+    }
+}
