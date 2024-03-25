2772212dd7703bdfc898d9af1954eed348b35fd4
Jae-Young98 <ygy0102@gmail.com>
2024-03-26 01:09:38 +0900

    숫자고르기

---
+++ src/BOJ/PrepareCodingTest/BOJ_2668.java
@@ -0,0 +1,52 @@
+package BOJ.PrepareCodingTest;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class BOJ_2668 {
+
+    static int[] arr;
+    static boolean[] visited;
+    static List<Integer> list;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        arr = new int[n + 1];
+
+        for (int i = 1; i <= n; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+
+        list = new ArrayList<>();
+        visited = new boolean[n + 1];
+        for (int i = 1; i <= n; i++) {
+            visited[i] = true;
+            dfs(i, i);
+            visited[i] = false;
+        }
+
+        Collections.sort(list);
+        System.out.println(list.size());
+        for (int i : list) {
+            System.out.println(i);
+        }
+    }
+
+    public static void dfs(int start, int target) {
+        if (!visited[arr[start]]) {
+            visited[arr[start]] = true;
+            dfs(arr[start], target);
+            visited[arr[start]] = false;
+        }
+
+        if (arr[start] == target) {
+            list.add(target);
+        }
+    }
+}
