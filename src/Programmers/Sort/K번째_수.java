1b29d09df3b87d057066b3818dd13f19f2d719e9
Jae-Young98 <86467141+Jae-Young98@users.noreply.github.com>
2024-04-12 20:39:51 +0900

    K번째 수

---
+++ src/Programmers/Sort/K번째_수.java
@@ -0,0 +1,39 @@
+package Programmers.Sort;
+
+import java.util.Arrays;
+
+public class K번째_수 {
+    public static int[] solution(int[] array, int[][] commands) {
+        int cmdLength = commands.length;
+        int[] answer = new int[cmdLength];
+
+        int i;
+        int j;
+        int k;
+        for (int c = 0; c < cmdLength; c++) {
+            i = commands[c][0] - 1;
+            j = commands[c][1] - 1;
+            k = commands[c][2] - 1;
+            int[] arr = new int[j - i + 1];
+
+            int idx = 0;
+            for (int x = i; x <= j; x++) {
+                arr[idx] = array[x];
+                idx++;
+            }
+
+            Arrays.sort(arr);
+            answer[c] = arr[k];
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        int[] array = {1, 5, 2, 6, 3, 7, 4};
+        int[][] commands = {{2, 5, 3}, {4, 4, 1}, {1, 7, 3}};
+        int[] result = solution(array, commands);
+
+        System.out.println(Arrays.toString(result));
+    }
+}
