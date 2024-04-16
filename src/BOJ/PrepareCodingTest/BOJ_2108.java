aa3b250c7f5f3b31d8e9185a0480b09dedb15b60
Jae-Young98 <ygy0102@gmail.com>
2024-04-16 23:45:22 +0900

    통계학

---
+++ src/BOJ/PrepareCodingTest/BOJ_2108.java
@@ -0,0 +1,54 @@
+package BOJ.PrepareCodingTest;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class BOJ_2108 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[n];
+        Map<Integer, Integer> map = new HashMap<>();
+
+        int sum = 0;
+        for (int i = 0; i < n; i++) {
+            int num = Integer.parseInt(br.readLine());
+            map.put(num, map.getOrDefault(num, 0) + 1);
+            arr[i] = num;
+            sum += num;
+        }
+
+        // 산술평균
+        double avg = (double) sum / n;
+        sb.append((int) Math.round(avg)).append('\n');
+
+        // 중앙값
+        Arrays.sort(arr);
+        sb.append(arr[n / 2]).append('\n');
+
+        // 최빈값
+        List<Integer> keySet = new ArrayList<>(map.keySet());
+        keySet.sort(new Comparator<Integer>() {
+            @Override
+            public int compare(Integer o1, Integer o2) {
+                return map.get(o2) - map.get(o1);
+            }
+        });
+        sb.append(keySet.get(0)).append('\n');
+
+        // 범위
+        sb.append(arr[n - 1] - arr[0]).append('\n');
+
+        System.out.print(sb);
+    }
+}
