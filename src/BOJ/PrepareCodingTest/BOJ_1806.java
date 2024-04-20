d372a95caac0fbd485175c4192933727ca377170
Jae-Young98 <ygy0102@gmail.com>
2024-04-20 22:42:22 +0900

    부분합

---
+++ src/BOJ/PrepareCodingTest/BOJ_1806.java
@@ -0,0 +1,47 @@
+package BOJ.PrepareCodingTest;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ_1806 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        int[] arr = new int[n + 1];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int s = 0;
+        int e = 0;
+        int result = Integer.MAX_VALUE;
+        int sum = 0;
+        while (s <= n && e <= n) {
+            if (sum >= m && result > e - s) {
+                result = e - s;
+            }
+
+            if (sum < m) {
+                sum += arr[e];
+                e++;
+            } else {
+                sum -= arr[s];
+                s++;
+            }
+        }
+
+        if (result == Integer.MAX_VALUE) {
+            System.out.println(0);
+        } else {
+            System.out.println(result);
+        }
+    }
+}
