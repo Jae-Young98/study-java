5a2fd10704727b84486f5a9cae2ad5ec9edbc93e
Jae-Young98 <ygy0102@gmail.com>
2024-05-06 20:43:21 +0900

    먹을 것인가 먹힐 것인가

---
+++ src/BOJ/PrepareCodingTest/BOJ_7795.java
@@ -0,0 +1,54 @@
+package BOJ.PrepareCodingTest;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class BOJ_7795 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+
+        int T = Integer.parseInt(br.readLine());
+
+        for (int t = 0; t < T; t++) {
+            st = new StringTokenizer(br.readLine());
+            int n = Integer.parseInt(st.nextToken());
+            int m = Integer.parseInt(st.nextToken());
+
+            int[] a = new int[n];
+            int[] b = new int[m];
+
+            st = new StringTokenizer(br.readLine());
+            for (int i = 0; i < n; i++) {
+                a[i] = Integer.parseInt(st.nextToken());
+            }
+
+            st = new StringTokenizer(br.readLine());
+            for (int i = 0; i < m; i++) {
+                b[i] = Integer.parseInt(st.nextToken());
+            }
+
+            Arrays.sort(a);
+            Arrays.sort(b);
+
+            int count = 0;
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < m; j++) {
+                    if (a[i] > b[j]) {
+                        count++;
+                    } else {
+                        break;
+                    }
+                }
+            }
+            sb.append(count).append('\n');
+        }
+
+        System.out.print(sb);
+    }
+}
