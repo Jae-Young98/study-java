2179f07c4c3c1d68e14984fd095db9c0a5d920a5
Jae-Young98 <ygy0102@gmail.com>
2024-05-14 19:34:48 +0900

    CD

---
+++ src/BOJ/PrepareCodingTest/BOJ_4158.java
@@ -0,0 +1,45 @@
+package BOJ.PrepareCodingTest;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ_4158 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        int[] a = new int[n];
+        int[] b = new int[m];
+
+        for (int i = 0; i < n; i++) {
+            a[i] = Integer.parseInt(br.readLine());
+        }
+
+        for (int i = 0; i < n; i++) {
+            b[i] = Integer.parseInt(br.readLine());
+        }
+
+        int l = 0;
+        int r = 0;
+        int result = 0;
+        while (l != n && r != m) {
+            if (a[l] == b[r]) {
+                result++;
+                l++;
+                r++;
+            } else if (a[l] > b[r]) {
+                r++;
+            } else {
+                l++;
+            }
+        }
+
+        System.out.println(result);
+    }
+}
