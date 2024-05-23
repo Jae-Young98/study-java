3f6e81860bd19497ff15b8d96baef0dc009d606b
Jae-Young98 <ygy0102@gmail.com>
2024-05-23 18:14:43 +0900

    행복 유치원

---
+++ src/BOJ/PrepareCodingTest/BOJ_13164.java
@@ -0,0 +1,38 @@
+package BOJ.PrepareCodingTest;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class BOJ_13164 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+
+        int[] arr = new int[n];
+        int[] diff = new int[n - 1];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        for (int i = 0; i < n - 1; i++) {
+            diff[i] = arr[i + 1] - arr[i];
+        }
+        Arrays.sort(diff);
+
+        int result = 0;
+        for (int i = 0; i < (n - 1) - (k - 1); i++) {
+            result += diff[i];
+        }
+
+        System.out.println(result);
+    }
+}
