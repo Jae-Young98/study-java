443da9ab538dfefbb9e5af4927db5411d9387ec0
Jae-Young98 <ygy0102@gmail.com>
2024-05-24 23:01:10 +0900

    안테나

---
+++ src/BOJ/PrepareCodingTest/BOJ_18310.java
@@ -0,0 +1,30 @@
+package BOJ.PrepareCodingTest;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class BOJ_18310 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[n];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr);
+        if (n % 2 == 0) {
+            System.out.println(arr[(n / 2) - 1]);
+        } else {
+            System.out.println(arr[n / 2]);
+        }
+    }
+}
