0ef6aa5d9ea30d43146647d86e5cfcfb0e837f45
Jae-Young98 <ygy0102@gmail.com>
2024-06-12 22:55:48 +0900

    바구니 뒤집기

---
+++ src/BOJ/PrepareCodingTest/BOJ_10811.java
@@ -0,0 +1,40 @@
+package BOJ.PrepareCodingTest;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ_10811 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        int[] arr = new int[n + 1];
+        for (int i = 1; i <= n; i++) {
+            arr[i] = i;
+        }
+
+        for (int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine());
+            int s = Integer.parseInt(st.nextToken());
+            int e = Integer.parseInt(st.nextToken());
+
+            while (s < e) {
+                int tmp = arr[s];
+                arr[s] = arr[e];
+                arr[e] = tmp;
+                s++;
+                e--;
+            }
+        }
+
+        for (int i = 1; i <= n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
