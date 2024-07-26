6202684da22ecf821dd7c7f795b12d92f74aaa12
Jae-Young98 <ygy0102@gmail.com>
2024-07-26 23:59:30 +0900

    과제 안 내신 분..?

--- src/BOJ/PrepareCodingTest/BOJ_5597.java
+++ src/BOJ/PrepareCodingTest/BOJ_5597.java
@@ -3,30 +3,25 @@ package BOJ.PrepareCodingTest;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 
 public class BOJ_5597 {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int[] arr = new int[28];
-        for (int i = 0; i < 28; i++) {
-            arr[i] = Integer.parseInt(br.readLine());
-        }
-        Arrays.sort(arr);
+        boolean[] arr = new boolean[31];
 
-        int x = 1;
-        int i = 0;
+        for (int i = 0; i < 28; i++) {
+            int x = Integer.parseInt(br.readLine());
+            arr[x] = true;
+        }
+
         int count = 0;
-        while (true) {
-            if (arr[i] != x) {
-                System.out.println(x);
+        for (int i = 1; i <= 30; i++) {
+            if (!arr[i]) {
+                System.out.println(i);
                 count++;
-            } else {
-                i++;
             }
-            x++;
 
             if (count == 2) {
                 return;
