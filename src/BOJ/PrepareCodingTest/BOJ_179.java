4f3d4bb096bd46426ce276bd059007fd65571fc1
Jae-Young98 <ygy0102@gmail.com>
2024-08-16 20:32:08 +0900

    백대열

---
+++ src/BOJ/PrepareCodingTest/BOJ_179.java
@@ -0,0 +1,31 @@
+package BOJ.PrepareCodingTest;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ_179 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), ":");
+
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        int gcd = gcd(n, m);
+        n /= gcd;
+        m /= gcd;
+
+        System.out.println(n + ":" + m);
+    }
+
+    public static int gcd(int a, int b) {
+        if (b == 0) {
+            return a;
+        }
+
+        return gcd(b, a % b);
+    }
+}
