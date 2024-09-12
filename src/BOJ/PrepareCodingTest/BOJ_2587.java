388f61401d2661c2a94f1b675168babf99ac05d5
Jae-Young98 <ygy0102@gmail.com>
2024-09-12 21:25:58 +0900

    대표값2

---
+++ src/BOJ/PrepareCodingTest/BOJ_2587.java
@@ -0,0 +1,24 @@
+package BOJ.PrepareCodingTest;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class BOJ_2587 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int[] arr = new int[5];
+        int avg = 0;
+        for (int i = 0; i < 5; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+            avg += arr[i];
+        }
+
+        Arrays.sort(arr);
+        System.out.println(avg / 5);
+        System.out.println(arr[2]);
+    }
+}
