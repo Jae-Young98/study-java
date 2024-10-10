722bcab9cbc005226d1b15baf6f8a888a569319f
Jae-Young98 <ygy0102@gmail.com>
2024-10-10 22:21:06 +0900

    사과 담기 게임

--- src/BOJ/PrepareCodingTest/BOJ_2828.java
+++ src/BOJ/PrepareCodingTest/BOJ_2828.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class BOJ_2628 {
+public class BOJ_2828 {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
