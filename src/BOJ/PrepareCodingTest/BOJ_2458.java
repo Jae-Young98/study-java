86a4155f2673fdd86d1ecc9472e82c85d5aac0d0
Jae-Young98 <ygy0102@gmail.com>
2024-10-12 20:17:04 +0900

    키순서

---
+++ src/BOJ/PrepareCodingTest/BOJ_2458.java
@@ -0,0 +1,72 @@
+package BOJ.PrepareCodingTest;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ_2458 {
+
+    static int INF = 999;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        int[][] arr = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                arr[i][j] = INF;
+
+                if (i == j) {
+                    arr[i][j] = 0;
+                }
+            }
+        }
+
+        for (int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken()) - 1;
+            int b = Integer.parseInt(st.nextToken()) - 1;
+
+            arr[a][b] = 1;
+        }
+
+        for (int k = 0; k < n; k++) {
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < n; j++) {
+                    arr[i][j] = Math.min(arr[i][j], arr[i][k] + arr[k][j]);
+                }
+            }
+        }
+
+        int result = 0;
+        int[] path = new int[n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i == j) {
+                    continue;
+                }
+
+                if (arr[i][j] < INF) {
+                    path[i]++;
+                    path[j]++;
+
+                    if (path[i] == n - 1) {
+                        result++;
+                    }
+
+                    if (path[j] == n - 1) {
+                        result++;
+                    }
+                }
+            }
+        }
+
+        System.out.println(result);
+    }
+}
