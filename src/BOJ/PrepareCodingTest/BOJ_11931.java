4d49ca5a1bbc3d288373c5d80174beb93fa68d7c
Jae-Young98 <ygy0102@gmail.com>
2024-11-12 23:34:41 +0900

    수 정렬하기 4

---
+++ src/BOJ/PrepareCodingTest/BOJ_11931.java
@@ -0,0 +1,28 @@
+package BOJ.PrepareCodingTest;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class BOJ_11931 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int n = Integer.parseInt(br.readLine());
+        List<Integer> list = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            list.add(Integer.parseInt(br.readLine()));
+        }
+
+        list.sort(Collections.reverseOrder());
+        for (int i : list) {
+            sb.append(i).append('\n');
+        }
+        System.out.print(sb);
+    }
+}
