2b19f4e61fef260a55bce7b44b13fb19fac16b81
Jae-Young98 <ygy0102@gmail.com>
2024-12-21 22:17:16 +0900

    수열 정렬

---
+++ src/BOJ/PrepareCodingTest/BOJ_1015.java
@@ -0,0 +1,38 @@
+package BOJ.PrepareCodingTest;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class BOJ_1015 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+
+        int n = Integer.parseInt(br.readLine());
+        int[] a = new int[n];
+        int[] b = new int[n];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            a[i] = Integer.parseInt(st.nextToken());
+            b[i] = a[i];
+        }
+
+        Arrays.sort(b);
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (a[i] == b[j]) {
+                    sb.append(j).append(' ');
+                    b[j] = -1;
+                    break;
+                }
+            }
+        }
+        System.out.println(sb);
+    }
+}
