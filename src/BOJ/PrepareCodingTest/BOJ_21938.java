47dd23a54e9c1fe091edcc76edbc56e4f757c65b
Jae-Young98 <ygy0102@gmail.com>
2025-01-01 23:35:28 +0900

    영상처리 - bfs

---
+++ src/BOJ/PrepareCodingTest/BOJ_21938.java
@@ -0,0 +1,84 @@
+package BOJ.PrepareCodingTest;
+
+import java.awt.Point;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class BOJ_21938 {
+
+    static int n;
+    static int m;
+    static int[][] arr;
+    static boolean[][] visited;
+    static int[] dx = {0, 1, 0, -1};
+    static int[] dy = {1, 0, -1, 0};
+    static Queue<Point> queue;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        arr = new int[n][m];
+        visited = new boolean[n][m];
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < m; j++) {
+                int r = Integer.parseInt(st.nextToken());
+                int g = Integer.parseInt(st.nextToken());
+                int b = Integer.parseInt(st.nextToken());
+                arr[i][j] = (r + g + b) / 3;
+            }
+        }
+
+        int t = Integer.parseInt(br.readLine());
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (arr[i][j] >= t) {
+                    arr[i][j] = 255;
+                } else {
+                    arr[i][j] = 0;
+                }
+            }
+        }
+
+        int result = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (arr[i][j] == 255 && !visited[i][j]) {
+                    result++;
+                    bfs(i, j);
+                }
+            }
+        }
+
+        System.out.println(result);
+    }
+
+    public static void bfs(int x, int y) {
+        queue = new LinkedList<>();
+        queue.add(new Point(x, y));
+        visited[x][y] = true;
+
+        while (!queue.isEmpty()) {
+            Point p = queue.poll();
+
+            for (int i = 0; i < 4; i++) {
+                int nx = p.x + dx[i];
+                int ny = p.y + dy[i];
+
+                if (nx >= n || nx < 0 || ny >= m || ny < 0 || visited[nx][ny] || arr[nx][ny] == 0) {
+                    continue;
+                }
+
+                queue.add(new Point(nx, ny));
+                visited[nx][ny] = true;
+            }
+        }
+    }
+}
