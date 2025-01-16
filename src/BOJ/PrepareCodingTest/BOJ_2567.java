1fcb08b99c0e9159434cf92ad268ddefa5785578
Jae-Young98 <ygy0102@gmail.com>
2025-01-16 22:56:29 +0900

    색종이 - 2

---
+++ src/BOJ/PrepareCodingTest/BOJ_2567.java
@@ -0,0 +1,50 @@
+package BOJ.PrepareCodingTest;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ_2567 {
+
+    static int[] dx = {0, 1, 0, -1};
+    static int[] dy = {1, 0, -1, 0};
+    static int[][] arr = new int[101][101];
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        int n = Integer.parseInt(br.readLine());
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+
+            for (int j = x; j < x + 10; j++) {
+                for (int k = y; k < y + 10; k++) {
+                    arr[j][k] = 1;
+                }
+            }
+        }
+
+        int result = 0;
+        for (int i = 1; i <= 100; i++) {
+            for (int j = 1; j <= 100; j++) {
+                if (arr[i][j] == 1) {
+                    for (int d = 0; d < 4; d++) {
+                        int nx = i + dx[d];
+                        int ny = j + dy[d];
+
+                        if (nx >= 1 && nx <= 100 && ny >= 1 && ny <= 100 && arr[nx][ny] == 0) {
+                            result++;
+                        } else if (nx < 1 || nx > 100 || ny < 1 || ny > 100) {
+                            result++;
+                        }
+                    }
+                }
+            }
+        }
+        System.out.println(result);
+    }
+}
