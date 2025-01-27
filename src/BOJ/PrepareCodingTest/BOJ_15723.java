177bcb74f740b6e9d42e48bc8042850f58f154d5
Jae-Young98 <ygy0102@gmail.com>
2025-01-27 19:07:05 +0900

    n단 논법

---
+++ src/BOJ/PrepareCodingTest/BOJ_15723.java
@@ -0,0 +1,60 @@
+package BOJ.PrepareCodingTest;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class BOJ_15723 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+
+        int n = Integer.parseInt(br.readLine());
+        boolean[][] arr = new boolean[26][26];
+
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = st.nextToken().charAt(0) - 'a';
+            st.nextToken();
+            int b = st.nextToken().charAt(0) - 'a';
+
+            arr[a][b] = true;
+        }
+
+        for (int i = 0; i < 26; i++) {
+            for (int j = 0; j < 26; j++) {
+                for (int k = 0; k < 26; k++) {
+                    if (i == j || j == k || k == i) {
+                        continue;
+                    }
+
+                    // i = k, k = j면 i = j
+                    if (arr[i][k] && arr[k][j]) {
+                        arr[i][j] = true;
+                    }
+                }
+            }
+        }
+
+        int m = Integer.parseInt(br.readLine());
+        for (int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = st.nextToken().charAt(0) - 'a';
+            st.nextToken();
+            int b = st.nextToken().charAt(0) - 'a';
+
+            if (arr[a][b]) {
+                sb.append("T").append('\n');
+            } else {
+                sb.append("F").append('\n');
+            }
+        }
+
+        System.out.print(sb);
+    }
+}
