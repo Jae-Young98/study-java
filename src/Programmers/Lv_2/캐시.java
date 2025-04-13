f6ce52ea2c9543d9938089d7513ef115ed79627f
Jae-Young98 <ygy0102@gmail.com>
2025-04-13 22:27:55 +0900

    캐시

---
+++ src/Programmers/Lv_2/캐시.java
@@ -0,0 +1,43 @@
+package Programmers.Lv_2;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class 캐시 {
+
+    public static void main(String[] args) {
+        System.out.println(solution(3, new String[]{"Jeju", "Pangyo", "Seoul", "NewYork", "LA", "Jeju", "Pangyo", "Seoul", "NewYork", "LA"}));
+        System.out.println(solution(3, new String[]{"Jeju", "Pangyo", "Seoul", "Jeju", "Pangyo", "Seoul", "Jeju", "Pangyo", "Seoul"}));
+        System.out.println(solution(2, new String[]{"Jeju", "Pangyo", "Seoul", "NewYork", "LA", "SanFrancisco", "Seoul", "Rome", "Paris", "Jeju", "NewYork", "Rome"}));
+        System.out.println(solution(5, new String[]{"Jeju", "Pangyo", "Seoul", "NewYork", "LA", "SanFrancisco", "Seoul", "Rome", "Paris", "Jeju", "NewYork", "Rome"}));
+        System.out.println(solution(0, new String[]{"Jeju", "Pangyo", "Seoul", "NewYork", "LA"}));
+    }
+
+    public static int solution(int cacheSize, String[] cities) {
+        int answer = 0;
+        List<String> cache = new ArrayList<>();
+
+        if (cacheSize == 0) {
+            return cities.length * 5;
+        }
+
+        for (int i = 0; i < cities.length; i++) {
+            String city = cities[i].toLowerCase();
+
+            if (cache.contains(city)) {
+                cache.remove(city);
+                cache.add(city);
+                answer += 1;
+            } else {
+                if (cache.size() == cacheSize) {
+                    cache.remove(0);
+                }
+
+                cache.add(city);
+                answer += 5;
+            }
+        }
+
+        return answer;
+    }
+}
