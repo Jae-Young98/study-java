e38ffbcf94390ab89f77000584ddba499cc64f92
Jae-Young98 <ygy0102@gmail.com>
2025-05-24 17:19:03 +0900

    내적

---
+++ src/Programmers/Lv_1/내적.java
@@ -0,0 +1,17 @@
+package Programmers.Lv_1;
+
+public class 내적 {
+
+    public static void main(String[] args) {
+        System.out.println(solution(new int[]{1, 2, 3, 4}, new int[]{-3, -1, 0, 2}));
+    }
+
+    public static int solution(int[] a, int[] b) {
+        int answer = 0;
+        for (int i = 0; i < a.length; i++) {
+            answer += (a[i] * b[i]);
+        }
+
+        return answer;
+    }
+}
